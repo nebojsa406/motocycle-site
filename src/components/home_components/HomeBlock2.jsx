268ec6cbf28ba750f72e2e626064742577799019
Nebojsa D <nebojsadubljevic406@gmail.com>
2025-12-21 20:36:42 +0100

    fix caregory block1

--- src/components/home_components/HomeBlock2.jsx
+++ src/components/home_components/HomeBlock2.jsx
@@ -4,17 +4,10 @@ import motor from "../../assets/home_block2/category-motor.png"
 import gear from "../../assets/home_block2/category-gear.png"
 import parts from "../../assets/home_block2/category-parts.png"
 
+function Category({ txt, img, side, name }) {
 
-
-export function HomeBlock2() {
-    const txt = `mooooootori mooooootori moootori moooootori motori moooootori mooooootori mooooootori moootori moooootori motori moooootori 
-    mooooootori mooooootori moootori moooootori motori moooootori mooooootori mooooootori moootori moooootori motori moooootori
-    mooooootori mooooootori moootori moooootori motori moooootori mooooootori mooooootori moootori `
-
-    function Category({ txt, img, side, name }) {
-
-        if(side=="R") {
-            return (
+    if (side == "R") {
+        return (
             <div className="category-left">
 
                 <img className="category-img" src={img} />
@@ -30,40 +23,45 @@ export function HomeBlock2() {
                 </div>
 
             </div>
-            
 
-            )
-        }
 
-        return (
-            <div className="category-right">
+        )
+    }
 
-                <div className="txt-side">
+    return (
+        <div className="category-right">
 
-                    <div className="category-title"> {name} </div>
+            <div className="txt-side">
 
-                    <div className="category-txt"> {txt}</div>
+                <div className="category-title"> {name} </div>
 
-                    <button className="shop-button"> SHOP </button>
+                <div className="category-txt"> {txt}</div>
 
-                </div>
-            
-                <img className="category-img" src={img} />
+                <button className="shop-button"> SHOP </button>
 
             </div>
-        )
-        
-    }
+
+            <img className="category-img" src={img} />
+
+        </div>
+    )
+
+}
+
+export function HomeBlock2() {
+    const txt = `mooooootori mooooootori moootori moooootori motori moooootori mooooootori mooooootori moootori moooootori motori moooootori 
+    mooooootori mooooootori moootori moooootori motori moooootori mooooootori mooooootori moootori moooootori motori moooootori
+    mooooootori mooooootori moootori moooootori motori moooootori mooooootori mooooootori moootori `
 
     return (
         <div className="Home-Block2">
             <div className="categorys-container">
-                
-                <Category txt={txt} name="MOTOR" side="L" img={motor}/>
-                <Category txt={txt} name="GEAR" side="R" img={gear}/>
-                <Category txt={txt} name="PARTS" side="L" img={parts}/>
-                <Category txt={txt} name="MOTOR" side="R" img={motor}/>
-            
+
+                <Category txt={txt} name="MOTOR" side="L" img={motor} />
+                <Category txt={txt} name="GEAR" side="R" img={gear} />
+                <Category txt={txt} name="PARTS" side="L" img={parts} />
+                <Category txt={txt} name="MOTOR" side="R" img={motor} />
+
             </div>
         </div>
     );
