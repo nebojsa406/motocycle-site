b9b2dc833da35889588aeda322bbdf02e2ec9e0b
kalajdzic383 <61320435+kalajdzic383@users.noreply.github.com>
2025-12-21 20:18:02 +0100

    fix

--- src/components/home_components/HomeBlock1.jsx
+++ src/components/home_components/HomeBlock1.jsx
@@ -1,6 +1,5 @@
 import "./HomeBlock1.css";
 import heroImage1 from "../../assets/home_block1/hero-image3-noBG.png";
-import ShopBtn from "../ShopBtn";
 
 export function HomeBlock1() {
     return (
@@ -8,8 +7,6 @@ export function HomeBlock1() {
             <button className="scroll-left-btn">{"<"}</button>
             <img className="hero-img" src={heroImage1} alt="motocycle"/>
             <button className="scroll-right-btn">{">"}</button>
-
-            <ShopBtn/>
 
         </div>
     );
